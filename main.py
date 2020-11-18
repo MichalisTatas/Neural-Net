797fc721b5004489741a75c1ba4ae031ff61fa1e
Fotiadis Stefanos <stefanos.fotiadis02@gmail.com>
2020-11-18 23:08:58 +0200

    activation function softmax

--- main.py
+++ main.py
@@ -22,38 +22,36 @@ def extract_data(filename):
 
 data, x, y = extract_data("./data/t10k-images-idx3-ubyte")
 
-print(data.shape)
 data = data.reshape(-1, x, y, 1)  # img_num * x * y * 1
-print(data.shape)
-print(data.dtype)
-
 data = data / np.max(data)
 
 train_X, valid_X, train_ground, valid_ground = train_test_split(
     data, data, test_size=0.25, shuffle=42)
 
+activationFunction = "softmax"
+
 
 def encoder(input_img):
-    conv1 = Conv2D(32, (3, 3), activation='relu',
+    conv1 = Conv2D(32, (3, 3), activation=activationFunction,
                    padding='same')(input_img)  # 28 x 28 x 32
     pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)  # 14 x 14 x 32
 
-    conv2 = Conv2D(64, (3, 3), activation='relu',
+    conv2 = Conv2D(64, (3, 3), activation=activationFunction,
                    padding='same')(pool1)  # 14 x 14 x 64
     pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)  # 7 x 7 x 64
 
-    conv3 = Conv2D(128, (3, 3), activation='relu',
+    conv3 = Conv2D(128, (3, 3), activation=activationFunction,
                    padding='same')(pool2)  # 7 x 7 x 128
 
     return conv3
 
 
 def decoder(input_conv):
-    conv3 = Conv2D(128, (3, 3), activation='relu',
+    conv3 = Conv2D(128, (3, 3), activation=activationFunction,
                    padding='same')(input_conv)  # 7 x 7 x 128
     up3 = UpSampling2D((2, 2))(conv3)  # 14 x 14 x 128
 
-    conv2 = Conv2D(64, (3, 3), activation='relu',
+    conv2 = Conv2D(64, (3, 3), activation=activationFunction,
                    padding='same')(up3)  # 14 x 14 x 64
     up2 = UpSampling2D((2, 2))(conv2)  # 28 x 28 x 64
 
@@ -79,7 +77,9 @@ print(autoencoder.summary())
 
 autoencoder_train = autoencoder.fit(train_X, train_ground, batch_size=batch_size,
                                     epochs=epochs, verbose=1, validation_data=(valid_X, valid_ground))
+autoencoder.save_weights("autoencoder_softmax_sigmoid.h5")
 
+# ploting loss graph
 loss = autoencoder_train.history['loss']
 val_loss = autoencoder_train.history['val_loss']
 epochs = range(epochs)
@@ -88,7 +88,8 @@ plt.plot(epochs, loss, 'bo', label='Training loss')
 plt.plot(epochs, val_loss, 'b', label='Validation loss')
 plt.title('Training and validation loss')
 plt.legend()
-plt.show()
+# plt.show()
+plt.savefig("loss_graph_softmax_sigmoid.png")
 
 # pred = autoencoder.predict(test_data)
 # plt.figure(figsize=(20, 4))
