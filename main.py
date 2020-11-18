3fc5515f3019298bed98b491eb1ff32d1a6f92c2
Fotiadis Stefanos <stefanos.fotiadis02@gmail.com>
2020-11-18 20:27:01 +0200

    extract_data

--- main.py
+++ main.py
@@ -1,40 +1,33 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 from sklearn.model_selection import train_test_split
-from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.layers import Conv2D, BatchNormalization
 
-f = open("./data/t10k-images-idx3-ubyte", "rb")
 
-f.read(4)
-data_n = int.from_bytes(f.read(4), "big", signed=True)
-x = int.from_bytes(f.read(4), "big", signed=True)
-y = int.from_bytes(f.read(4), "big", signed=True)
+def extract_data(filename):
+    with open(filename, "rb") as file:
+        file.read(4)
+        num_images = int.from_bytes(file.read(4), "big", signed=True)
+        x = int.from_bytes(file.read(4), "big", signed=True)
+        y = int.from_bytes(file.read(4), "big", signed=True)
+        buf = file.read(num_images * x * y)
+        data = np.frombuffer(buf, dtype=np.uint8)
+        data = data.reshape(num_images, x, y)
 
-data = []
-for i in range(0, data_n):
-    point = []
+        return data
 
-    for j in range(0, x*y):
-        byte = f.read(1)
 
-        if byte != b"":
-            point.append(int.from_bytes(byte, "little"))
+data = extract_data("./data/t10k-images-idx3-ubyte")
 
-    data.append(point)
-f.close()
+# Display the first image in training data
+plt.figure(figsize=[5, 5])
+plt.subplot(121)
+curr_img = np.reshape(data[0], (28, 28))
+plt.imshow(curr_img, cmap='gray')
+plt.show()
 
-data = np.array(data, dtype=np.uint8)
-print(data.shape)
 
 train, test = train_test_split(data)
 print(train.shape)
 print(test.shape)
-
-
-def encoder(image):
-    conv1 = Conv2D(32, (3, 3), activation="relu", padding="same")(image)
-
-    return conv1
-
-
-encoder(train[0])
